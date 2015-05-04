8b3b3c35f40e24e63048f8315ebba03e39ece929
Aline - L <aline.legros80@gmail.com>
2015-05-04 11:26:43 +0200

    Gestion type user/webmaster pour l'ajout d'article + modification  menu

--- index.php
+++ index.php
@@ -28,54 +28,58 @@
 						$result = $bdd->query("SELECT MAX(Numero_Article) AS MaxNum FROM article ");
 						$donnee= $result->fetch();
 						$num_Article=$donnee['MaxNum'];
-						
-						$query1 = $bdd->query('SELECT Titre, Chemin_Resume, Redacteur, Date_Ajout, Chemin_Image FROM article ORDER BY Numero_Article DESC');
-						while ($donnees = $query1->fetch())
-						{
-							echo "<article>";
-								echo '<h1>'.$donnees['Titre'].' | posté par '.$donnees['Redacteur'].' le '.$donnees['Date_Ajout'].'</h1>';
-								echo '<div class="image_article">';
-									echo '<img src="'.$donnees['Chemin_Image'].'" alt="chat" width=300px height=300px>';
-								echo "</div>";
-								
-								echo '<div class="texte_article">';
-									echo "<section>";
-										echo '<h2>'.$donnees['Titre'].'</h2>';
-										$fichier=fopen($donnees['Chemin_Resume'],'r');
-										if($fichier!=null){
-											$ligne=fgets($fichier);
-											echo '<p>';
-											while($ligne){
-												echo $ligne;
-												$ligne=fgets($fichier);
-											}
-											fclose($fichier);
-											echo '</p>';
-										}
-										echo '<nav><a href="article.php?id='.$num_Article.'"> Lire la suite... </a></nav>';
-									echo "</section>";
-									
-									echo "<section>";
-										echo "<h2>Dernier commentaire sur cet article</h2>";
-								
-										$query2 = $bdd->prepare('SELECT Identifiant, Date_Redaction, Contenu FROM commentaire WHERE Numero_Article=? AND Date_Redaction=
-											(SELECT MAX(Date_Redaction) FROM commentaire WHERE Numero_Article=?)');
-										$query2->execute(array($num_Article,$num_Article));
-										$nbCommentaires=$query2->rowCount();
-										if($nbCommentaires==0) {
-											echo "<p>Il n' y a pas encore de commentaire sur cet article.</p>";
-										}
-										else{
-											$commentaire=$query2->fetch();
-											echo '<p><q>'.$commentaire['Contenu'].'</q> - par '.$commentaire['Identifiant'].' le '.$commentaire['Date_Redaction'].'</p>';
-										}
-										$query2->closeCursor();
-									echo "</section>";
-								echo "</div>";
-							echo "</article>";
-						$num_Article=$num_Article-1;	
+						if($num_Article==0){
+							echo("<p>Il n'y a pas encore d'article sur ce blog!</p>");
 						}
-					$query1->closeCursor();
+						else{
+								$query1 = $bdd->query('SELECT Titre, Chemin_Resume, Redacteur, Date_Ajout, Chemin_Image FROM article ORDER BY Numero_Article DESC');
+								while ($donnees = $query1->fetch())
+								{
+									echo "<article>";
+										echo '<h1>'.$donnees['Titre'].' | posté par '.$donnees['Redacteur'].' le '.$donnees['Date_Ajout'].'</h1>';
+										echo '<div class="image_article">';
+											echo '<img src="'.$donnees['Chemin_Image'].'" alt="chat" width=300px height=300px>';
+										echo "</div>";
+										
+										echo '<div class="texte_article">';
+											echo "<section>";
+												echo '<h2>'.$donnees['Titre'].'</h2>';
+												$fichier=fopen($donnees['Chemin_Resume'],'r');
+												if($fichier!=null){
+													$ligne=fgets($fichier);
+													echo '<p>';
+													while($ligne){
+														echo $ligne;
+														$ligne=fgets($fichier);
+													}
+													fclose($fichier);
+													echo '</p>';
+												}
+												echo '<nav><a href="article.php?id='.$num_Article.'"> Lire la suite... </a></nav>';
+											echo "</section>";
+											
+											echo "<section>";
+												echo "<h2>Dernier commentaire sur cet article</h2>";
+										
+												$query2 = $bdd->prepare('SELECT Identifiant, Date_Redaction, Contenu FROM commentaire WHERE Numero_Article=? AND Date_Redaction=
+													(SELECT MAX(Date_Redaction) FROM commentaire WHERE Numero_Article=?)');
+												$query2->execute(array($num_Article,$num_Article));
+												$nbCommentaires=$query2->rowCount();
+												if($nbCommentaires==0) {
+													echo "<p>Il n' y a pas encore de commentaire sur cet article.</p>";
+												}
+												else{
+													$commentaire=$query2->fetch();
+													echo '<p><q>'.$commentaire['Contenu'].'</q> - par '.$commentaire['Identifiant'].' le '.$commentaire['Date_Redaction'].'</p>';
+												}
+												$query2->closeCursor();
+											echo "</section>";
+										echo "</div>";
+									echo "</article>";
+								$num_Article=$num_Article-1;	
+								}
+								$query1->closeCursor();
+							}
 				?>
 			</div>
 
